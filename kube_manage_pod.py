b421743a953956a33ede836cb247ad51ed357b09
dwchoo <dwchoo1991@gmail.com>
2021-10-20 19:52:14 +0900

    FIX kube_manage_pod's bug

--- kube_manage_pod.py
+++ kube_manage_pod.py
@@ -16,11 +16,11 @@ class pod_checker:
 
         self.bool_system_namespace = pod_checker.check_system_namespace(self.pod)
         self.bool_restart_threshold = self.check_restart_count(self.pod)
-        self.bool_error_message = self.check_error_message
+        self.bool_error_message = self.check_error_message(self.pod)
         self.bool_forbidden_command = self.check_forbidden_command(self.pod)
 
         self.namespace = self.return_namespace(self.pod)
-        self.pod_name = self.pod_name(self.pod)
+        self.pod_name = self.return_pod_name(self.pod)
 
     def check_kill(self,):
         kill_policy_list = [
@@ -70,12 +70,13 @@ class pod_checker:
             if any([__command in _args for _args in args]):
                 self.break_command = __command
                 return True
+        self.break_command = 'None'
         return False
 
     def check_restart_count(self,i):
         threshold = pod_checker.RESTART_THRESHOLD
         try:
-            self.restart_count = int(i.status.container_status[0].restart_count)
+            self.restart_count = int(i.status.container_statuses[0].restart_count)
         except:
             self.restart_count = 0
         if self.restart_count > threshold:
@@ -100,7 +101,7 @@ class pod_checker:
     @classmethod
     def container_status(cls,i):
         try:
-            status = i.status.container_status
+            status = i.status.container_statuses
         except:
             status = None
         return status
@@ -140,26 +141,29 @@ pod_checker.FORBIDDEN_COMMAND = FORBIDDEN_COMMAND
 pod_checker.ERROR_MESSAGE = ERROR_MESSAGE
 
 
-config.load_kube_config()
-v1 = client.CoreV1Api()
-ret = v1.list_pod_for_all_namespaces()
+if __name__ == '__main__':
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+    ret = v1.list_pod_for_all_namespaces()
+    #ret = v1.list_namespaced_pod('')
 
 
-for i in ret.items:
-    #if pod_checker.check_system_namespace(i)
-    if pod_checker.check_system_namespace(i):
-        continue	# if it is system namespace, continue(pass below code).
+    for i in ret.items:
+        if pod_checker.check_system_namespace(i):
+            continue	# if it is system namespace, continue(pass below code).
 
-    if pod_checker.container_status(i):
-        _pod_check = pod_checker(i)
-        _namespace = _pod_check.namespace
-        _pod_name = _pod_check.pod_name
-        # Check pod
-        kill_policy = any(_pod_check.check_kill())
-        pprint(_pod_check.pod_info()['log'])
-        pprint(_pod_check.check_kill())
-        # delete pod
-        #if kill_policy:
-        #    v1.delete_namespaced_pod(name=_pod_name,namespace=_namespace)
-        #    print(f'kill pod:{_pod_name}, namespace:{_namespace}')
+        if pod_checker.container_status(i):
+            _pod_check = pod_checker(i)
+            _namespace = _pod_check.namespace
+            _pod_name = _pod_check.pod_name
+            
+            # Check pod
+            kill_policy = any(_pod_check.check_kill())
+            #pprint(_pod_check.pod_info()['log'])
+            #pprint(_pod_check.check_kill())
+
+            # delete pod
+            if kill_policy:
+                v1.delete_namespaced_pod(name=_pod_name,namespace=_namespace)
+                print(f'kill pod:{_pod_name}, namespace:{_namespace}')
                     
